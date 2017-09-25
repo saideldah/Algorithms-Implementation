74e28daa037849538466875ebfe52e13334aa0e8
Said El Dah <S.elDah@Bsynchro.com>
2017-09-25 19:05:17 +0300

    camel Case

---
+++ CamelCase/CamelCase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int count = 0;
+            if (!string.IsNullOrWhiteSpace(s))
+            {
+                count = 1;
+                for (char i = 'A'; i <= 'Z'; i++)
+                {
+                    count += s.Count(x => x == i);
+                }
+            }
+            System.Console.WriteLine(count);
+        }
+    }
+}
